24033ad0bc6d69633165c96e6ba91df8c0bdd5c2
oqj <654570524@qq.com>
2017-07-30 07:50:06 +0800

    continue code

--- mysite/jizhangben/views.py
+++ mysite/jizhangben/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from  django.http import  HttpResponse
 from  .models import  Question
+from django.http import  Http404
 from django.template import RequestContext,loader
 # Create your views here.
 def index(request):
@@ -17,8 +18,8 @@ def results(request,question_id):
     return  HttpResponse(response%question_id)
 
 def details(request,question_id):
-    response="You're looking at the details of question %s."
-    return  HttpResponse(response%question_id)
+   question=get_object_or_404(Question,pk=question_id)
+   return  render(request,"jizhangben/detail.html",{'question':question})
 
 def vote(request,question_id):
     return HttpResponse("You're voting on question %s." % question_id)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^jizhangben/',include('jizhangben.urls')),
+    url(r'^jizhangben/',include('jizhangben.urls',namespace='jizhangben')),
 ]
